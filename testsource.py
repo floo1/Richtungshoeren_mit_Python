0edaf59f11375c60818076069f6aca97b46081dd
floo1 <dietl2003@gmx.de>
2013-11-12 19:14:34 +0100

    Create testsource.py

---
+++ testsource.py
@@ -0,0 +1,12 @@
+def halo():
+    print ("geht nicht")
+#    return(30 + 5)
+
+def addieren(a,b):
+#    a=3
+#    b=4
+    y=a-b
+    print("%f plus %f  ergibt %f" %(a,b,y))
+    # return (y)
+
+print ("guten Tag")
