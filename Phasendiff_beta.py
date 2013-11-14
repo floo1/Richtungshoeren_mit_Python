d0eb5f108033c1c857b52dd588a18e1f0e614da3
Franzus <franz-wichert@t-online.de>
2013-11-15 00:10:32 +0100

    Phasendiff_beta-Änderung vom Treffen 14.11.13 / SoundcardSkript ".py"-Endung angehängt

--- Phasendiff_beta.py
+++ Phasendiff_beta.py
@@ -9,14 +9,29 @@ from pylab import *
 from scipy import *
 
 
+# Pegelschwelle definieren, durch die das Grundrauschen ignoriert werden soll
+pegelschwelle = -100
+
 # Erstellen des Testsignalvektors "stereo_vec"
 #######################################
 fs = 44100
 dur = 1
 t = arange(0,dur,1/fs)
-f = 500
+	### Stereovektor mit Pegel-/Phasenunterschiedlicher Sinus ###
+'''
+f = 5000
 sig_li = sin(2*pi*f*t)
 sig_re = 2*sin(2*pi*f*t+pi/2)
+'''
+	### Stereovektor mit gleichen um 1 Sample verschobenes Rauschen auf beiden Kanälen ###
+
+sig_main = np.random.randn(len(t))
+sig_li = sig_main
+
+n_delay = 6
+sig_re = np.append(sig_main[n_delay:(len(sig_main))],sig_main[0:n_delay])
+#sig_re = sig_re[0:len(sig_li)]
+
 
 stereo_vec = zeros((len(sig_li),2))
 stereo_vec[:,0] = sig_li
@@ -31,7 +46,7 @@ block_leng = len(stereo_vec)
 f_vec = (arange(block_leng)/block_leng)*Fs	# f_vec enthält n-viele Frequenzen von 0 bis < Fs 
 f_vec = f_vec[0:int(block_leng/2)] 			# f_vec von Fs auf Fs/2 teilen, wobei nicht sicher ob abrunden(n/2) oder aufrunden(n/2)
 
-fft_y = np.fft.fft(stereo_vec, axis = 0)
+fft_y = (1/(fs*dur))*np.fft.fft(stereo_vec, axis = 0)	#(1/(fs*dur)) entspricht 1/N bei Fouriertransformation; => Python macht FFT ohne Faktor 1/N
 fft_y = fft_y[0:int(block_leng/2)] 
 fft_Pegel = 20*log10(abs(fft_y))
 #######################################
@@ -65,20 +80,25 @@ re_Winkel = np.zeros((len(fft_y),1))
 li_Pegel = np.zeros((len(fft_y),1))
 re_Pegel = np.zeros((len(fft_y),1))
 
+# Alle fft-Werte, die als Pegel weniger als -100 dB ergeben werden auf 1 gesetzt
+fft_y_li = [1 if 20*log10(abs(i)) < pegelschwelle else i for i in fft_y[:,0]]
+fft_y_re = [1 if 20*log10(abs(i)) < pegelschwelle else i for i in fft_y[:,1]]
+
 
 for m in range(0,len(fft_y),1):
 	# Winkel berechnen
-	li_Winkel[m,0] = math.atan(fft_y[m,0].imag/fft_y[m,0].real)
-	re_Winkel[m,0] = math.atan(fft_y[m,1].imag/fft_y[m,1].real)
+	li_Winkel[m,0] = angle(fft_y_li[m])
+	re_Winkel[m,0] = angle(fft_y_re[m])
 	# Pegel berechnen
-	li_Pegel[m,0] = fft_Pegel[m,0]
-	re_Pegel[m,0] = fft_Pegel[m,1]
+	li_Pegel[m,0] = 20*log10(abs(fft_y_li[m]))
+	re_Pegel[m,0] = 20*log10(abs(fft_y_re[m]))
 	
 	
 # Winkeldiff berechnen	
-Diff_Winkel = abs(li_Winkel - re_Winkel)
+Diff_Winkel = li_Winkel - re_Winkel
+Diff_Winkel = np.unwrap(Diff_Winkel, discont = np.pi, axis = 0)		#unwrap verhindert Phasensprünge die über 2pi hinausgehen
 # Pegeldiff berechnen
-Diff_Pegel = abs(li_Pegel - re_Pegel)
+Diff_Pegel = li_Pegel - re_Pegel
 
 #########################################
 
