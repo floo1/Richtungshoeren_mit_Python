02f3abd70b2310c0f751e1219ed3706a4b7a9f84
floo1 <dietl2003@gmx.de>
2013-11-14 17:23:36 +0100

    Create testsource.py

---
+++ testsource.py
@@ -0,0 +1,7 @@
+def addieren(a,b):
+# a=3
+# b=4
+    y=a-b
+    x=b-a
+  #  print("Die Koeffizienten sind a= %f und b= %f, a minus b ergibt den Wert y= %f, b minus a ergibt x=%f." %(a,b,y,x))
+    return (y,x)
